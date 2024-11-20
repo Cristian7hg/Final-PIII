95410e8494854863b843045f95103e37a0d0a759
JosephFrost <josephnina16forjob@gmail.com>
2024-11-20 18:26:15 -0400

    Reintentando el uso de sqlite

--- server/PedidosYa.Data/database/conexion.js
+++ server/PedidosYa.Data/database/conexion.js
@@ -8,21 +8,21 @@ const fs = require("fs");
 
 // La contrasena debe ser recibeya
 
-const sequelize = new Sequelize('recibe_ya','root','Frosty16', {
-    host:'localhost',
-    dialect:'mysql',
-    logging: false 
-});
+// const sequelize = new Sequelize('recibe_ya','root','Frosty16', {
+//     host:'localhost',
+//     dialect:'mysql',
+//     logging: false 
+// });
 
-// const dbPath = path.join(
-//     path.dirname(require.main.filename),
-//     "PedidosYa.Data",
-//     "database"
-//   );
+const dbPath = path.join(
+    path.dirname(require.main.filename),
+    "PedidosYa.Data",
+    "database"
+  );
   
-// const sequelize = new Sequelize({
-//     dialect: "sqlite",
-//     storage: path.join(dbPath,"PedidosYa.sqlite")
-//   });
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(dbPath,"PedidosYa.sqlite")
+  });
 
 module.exports = sequelize;
