d1e087764743f2bb65779ca67465fbd0f9c9f37e
JosephFrost <josephnina16forjob@gmail.com>
2024-11-20 17:27:59 -0400

    Cambio de base de datos a una tipo sqlite

--- server/PedidosYa.Data/database/conexion.js
+++ server/PedidosYa.Data/database/conexion.js
@@ -1,15 +1,16 @@
 const { Sequelize } = require("sequelize");
+const path = require("path");
+const fs = require("fs");
 
-// El nombre de la base de datos debe ser recibe_ya 
-
-// El usuario debe ser root
-
-// La contrasena debe ser recibeya
-
-const sequelize = new Sequelize('recibe_ya','root','reyesledesma11', {
-    host:'localhost',
-    dialect:'mysql',
-    logging: false 
-});
+const dbPath = path.join(
+    path.dirname(require.main.filename),
+    "PedidosYa.Data",
+    "database"
+  );
+  
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(dbPath,"PedidosYa.sqlite")
+  });
 
 module.exports = sequelize;
